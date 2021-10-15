5d505428334fba1b2b81c680dad6f4c36d8f299b
lifei <876451336@qq.com>
2021-10-15 11:29:15 +0800

    删除wxparse相关

--- src/pages/app/article.vue
+++ src/pages/app/article.vue
@@ -1,21 +1,16 @@
 <template>
 	<div class="node-article">
 		<towxml :nodes="result" />
-		<block v-if="html || result">
-			<!-- <wxParse className="node-html" :content="html" :imageProp="{ mode: 'widthFix' }"></wxParse> -->
-		</block>
 	</div>
 </template>
 
 <script>
-import wxParse from 'mpvue-wxparse'
-import marked from 'marked'
 import towxml from '../../static/towxml/towxml'
 import { HttpStatus, nodeClientArticle } from '@/api'
 
 export default {
 	name: 'Article',
-	components: { wxParse, towxml },
+	components: { towxml },
 	data() {
 		return {
 			html: '',
@@ -32,15 +27,9 @@ export default {
 			try {
 				const { code, data } = await nodeClientArticle({ id })
 				if (code === HttpStatus.OK) {
-					// this.html = marked(data.html)
+					this.html = data.html
 					this.result = this.towxml(data.content, 'markdown', {
-						theme: 'light', // 主题，默认`light`
-						events: {
-							// 为元素绑定的事件方法
-							tap: e => {
-								console.log('tap', e)
-							}
-						}
+						theme: 'light'
 					})
 				}
 			} catch (e) {}
@@ -52,30 +41,5 @@ export default {
 <style lang="scss">
 .node-article {
 	padding: 20rpx;
-}
-.h2w__mark {
-	display: inline-block;
-	color: #48b378;
-	padding: 2rpx 8rpx;
-	border-radius: 8rpx;
-	background-color: #ebebeb;
-	font-family: 'Operator Mono', Consolas, Monaco, Menlo, monospace;
-}
-.node-html {
-	.mark {
-		display: inline-block;
-		color: #48b378;
-		padding: 2rpx 8rpx;
-		border-radius: 8rpx;
-		background-color: #ebebeb;
-		font-family: 'Operator Mono', Consolas, Monaco, Menlo, monospace;
-	}
-	.blockquote {
-		.mark {
-			line-height: 1.4;
-			padding: 0 10rpx;
-			margin-left: 10rpx;
-		}
-	}
 }
 </style>
