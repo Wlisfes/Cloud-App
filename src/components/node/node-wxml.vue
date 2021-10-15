0310cb9d4130ddea01b538ffc886d0a100465324
lifei <876451336@qq.com>
2021-10-15 13:05:41 +0800

    新增富文本渲染组件

---
+++ src/components/node/node-wxml.vue
@@ -0,0 +1,24 @@
+<template>
+	<towxml :nodes="html"></towxml>
+</template>
+
+<script>
+import towxml from '../../static/towxml/towxml'
+export default {
+	name: 'NodeWxml',
+	components: { towxml },
+	props: {
+		content: {
+			type: String,
+			default: ''
+		}
+	},
+	computed: {
+		html() {
+			return this.towxml(this.content, 'markdown', {
+				theme: 'light'
+			})
+		}
+	}
+}
+</script>
--- src/pages/app/article.vue
+++ src/pages/app/article.vue
@@ -1,20 +1,17 @@
 <template>
 	<div class="node-article">
-		<towxml :nodes="result" />
+		<node-wxml :content="content"></node-wxml>
 	</div>
 </template>
 
 <script>
-import towxml from '../../static/towxml/towxml'
 import { HttpStatus, nodeClientArticle } from '@/api'
 
 export default {
 	name: 'Article',
-	components: { towxml },
 	data() {
 		return {
-			html: '',
-			result: ''
+			content: ''
 		}
 	},
 	onLoad({ id, title }) {
@@ -25,13 +22,12 @@ export default {
 		/**文章信息-客户端**/
 		async initNodeClientArticle(id) {
 			try {
+				uni.showLoading({ title: '加载中' })
 				const { code, data } = await nodeClientArticle({ id })
 				if (code === HttpStatus.OK) {
-					this.html = data.html
-					this.result = this.towxml(data.content, 'markdown', {
-						theme: 'light'
-					})
+					this.content = data.content
 				}
+				setTimeout(() => uni.hideLoading(), 500)
 			} catch (e) {}
 		}
 	}
