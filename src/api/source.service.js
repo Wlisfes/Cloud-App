53194f9d9674d3ec291c7f28851dca46d636f7b0
fanyanghua <876451336@qq.com>
2021-10-23 22:09:45 +0800

    添加标签相关接口

--- src/api/source.service.js
+++ src/api/source.service.js
@@ -1,10 +1,55 @@
 import request from '@/utils/request'
 
+/**创建标签**/
+export function nodeCreateSource(params) {
+	return request({
+		url: `/api/source/create`,
+		method: 'POST',
+		data: params
+	})
+}
+
+/**修改标签**/
+export function nodeUpdateSource(params) {
+	return request({
+		url: `/api/source/update`,
+		method: 'PUT',
+		data: params
+	})
+}
+
+/**切换标签状态**/
+export function nodeSourceCutover(params) {
+	return request({
+		url: `/api/source/cutover`,
+		method: 'PUT',
+		data: params
+	})
+}
+
+/**标签信息**/
+export function nodeSource(params) {
+	return request({
+		url: `/api/source/info`,
+		method: 'GET',
+		params
+	})
+}
+
 /**标签列表**/
 export function nodeSources(params) {
 	return request({
 		url: `/api/source/list-node`,
 		method: 'GET',
-		data: params
+		params
+	})
+}
+
+/**分类标签**/
+export function nodeDeleteSource(params) {
+	return request({
+		url: `/api/source/del`,
+		method: 'DELETE',
+		params
 	})
 }
